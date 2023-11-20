246d135064a1d92e3edca46b48d9f6be429d6c67
MansurTsutiev <mtsutiev@movableink.com>
2023-11-19 21:13:29 -0500

    add telescope fzf

--- lua/plugin_configs/telescope.lua
+++ lua/plugin_configs/telescope.lua
@@ -1,7 +1,31 @@
-------------------Telescope ---------------------------------------
+require('telescope').setup{
+  pickers = {
+    find_files = {
+      theme = "dropdown",
+      max_results = 100,
+    }
+  },
+  extensions = {
+    fzf = {
+      fuzzy = true,                    -- false will only do exact matching
+      override_generic_sorter = true,  -- override the generic sorter
+      override_file_sorter = true,     -- override the file sorter
+      case_mode = "smart_case",        -- or "ignore_case" or "respect_case"
+                                       -- the default case_mode is "smart_case"
+    },
+    file_browser = {
+      theme = "dropdown",
+      hijack_netrw = true, -- disabled native nvim file explorer
+    }
+  }
+}
+
 local builtin = require('telescope.builtin')
 vim.keymap.set('n', ',f', builtin.find_files, {})
 vim.keymap.set('n', ',fg', builtin.live_grep, {})
 vim.keymap.set('n', ',fe', ":Telescope file_browser<CR>", { noremap = true })
 
+
 require("telescope").load_extension "file_browser"
+require('telescope').load_extension('fzf')
+
--- lua/plugins.lua
+++ lua/plugins.lua
@@ -9,6 +9,7 @@ local plugins = {
     'stevearc/conform.nvim',
     "numToStr/Comment.nvim",
     { "lukas-reineke/indent-blankline.nvim", main = "ibl" },
+    { 'nvim-telescope/telescope-fzf-native.nvim', build = 'make' },
     {
       'nvim-telescope/telescope.nvim', tag = '0.1.4',
       dependencies = { 'nvim-lua/plenary.nvim' }
