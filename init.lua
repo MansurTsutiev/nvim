3c79eeb2dffaea30df56323ae1cfd3170bbd930d
MansurTsutiev <mtsutiev@movableink.com>
2024-04-10 16:49:00 -0400

    move the nvimtree into its own file

--- init.lua
+++ init.lua
@@ -17,30 +17,3 @@ require("plugin_configs")
 require("config.user_settings")
 
 
----------------------------------------------
--- TODO: move this into its own file
--- disable netrw at the very start of your init.lua
-vim.g.loaded_netrw = 1
-vim.g.loaded_netrwPlugin = 1
--- optionally enable 24-bit colour
-vim.opt.termguicolors = true
-
-require("nvim-tree").setup({
-  sort = {
-    sorter = "case_sensitive",
-  },
-  view = {
-    width = 30,
-  },
-  renderer = {
-    group_empty = true,
-  },
-  filters = {
-    dotfiles = true,
-  },
-  update_focused_file = {
-    enable = true,
-    exclude = false,
-  },
-})
----------------------------------------------
--- lua/plugin_configs/telescope.lua
+++ lua/plugin_configs/telescope.lua
@@ -22,7 +22,6 @@ telescope.setup{
 local builtin = require('telescope.builtin')
 vim.keymap.set('n', ',f', builtin.find_files, {})
 vim.keymap.set('n', ',fg', builtin.live_grep, {})
--- vim.keymap.set('n', ',fe', ":Telescope file_browser path=%:p:h select_buffer=true<CR>", { noremap = true })
 
 
 telescope.load_extension('fzf')
