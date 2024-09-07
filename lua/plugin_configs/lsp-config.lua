52aa15d2e4d47746d5ceaa9f43707a2b150fe8fe
MansurTsutiev <mtsutiev@movableink.com>
2024-09-07 16:00:41 -0400

    map lsp code actions

--- lua/plugin_configs/lsp-config.lua
+++ lua/plugin_configs/lsp-config.lua
@@ -4,14 +4,6 @@ require("mason-lspconfig").setup {
 }
 
 local lspconfig = require('lspconfig')
-lspconfig.lua_ls.setup({
-  settings = {
-    Lua = {
-      diagnostics = {
-        globals = {'vim'} -- supress warning for globalb vim not defined
-      }
-    }
-  }
-})
-
+lspconfig.lua_ls.setup({})
+vim.keymap.set({ 'n', 'v' }, '<leader>ca', vim.lsp.buf.code_action, {})
 
