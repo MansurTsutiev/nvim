d1518938c4d6af565139f4442b69b79dbd79eb12
MansurTsutiev <mtsutiev@movableink.com>
2024-09-07 15:42:05 -0400

    supress lua gloabl vim warning

--- lua/plugin_configs/lsp-config.lua
+++ lua/plugin_configs/lsp-config.lua
@@ -2,7 +2,16 @@ require("mason").setup()
 require("mason-lspconfig").setup {
   ensure_installed = { "lua_ls" },
 }
+
 local lspconfig = require('lspconfig')
-lspconfig.lua_ls.setup({})
+lspconfig.lua_ls.setup({
+  settings = {
+    Lua = {
+      diagnostics = {
+        globals = {'vim'} -- supress warning for globalb vim not defined
+      }
+    }
+  }
+})
 
 
