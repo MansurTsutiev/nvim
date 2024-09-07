488e2aa3d7cc7b07650a6e13543e30e96ed55f2e
MansurTsutiev <mtsutiev@movableink.com>
2024-09-07 13:53:22 -0400

    remove redundant dependency

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -1,6 +1,6 @@
 local plugins = {
     'nvim-tree/nvim-tree.lua',
-    'nvim-tree/nvim-web-devicons',
+    'nvim-tree/nvim-web-devicons', -- icons for nvimtree and barbar
     'drewtempelmeyer/palenight.vim',
     'tpope/vim-fugitive',
     'tpope/vim-rhubarb',
@@ -12,15 +12,9 @@ local plugins = {
     {'romgrk/barbar.nvim', -- tabs ui
       dependencies = {
         'lewis6991/gitsigns.nvim', -- OPTIONAL: for git status
-        'nvim-tree/nvim-web-devicons', -- OPTIONAL: for file icons
       },
       init = function() vim.g.barbar_auto_setup = false end,
-      opts = {
-        -- lazy.nvim will automatically call setup for you. put your options here, anything missing will use the default:
-        -- animation = true,
-        -- insert_at_start = true,
-        -- …etc.
-      },
+      opts = {},
       version = '^1.0.0', -- optional: only update when a new 1.x version is released
     },
 }
