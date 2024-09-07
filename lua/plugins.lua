cc9d622933f78fbae5b506c0841ef428de442ed8
MansurTsutiev <mtsutiev@movableink.com>
2024-09-07 15:36:04 -0400

    add neovim lspconfig

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -1,10 +1,13 @@
 local plugins = {
   { "catppuccin/nvim", name = "catppuccin", priority = 1000 }, -- theme
-  'nvim-tree/nvim-tree.lua',
-  'nvim-tree/nvim-web-devicons', -- icons for nvimtree and barbar
-  'tpope/vim-fugitive', -- Git commands (blame, browse)
-  'tpope/vim-rhubarb', -- opens github for GBrowse
-  "numToStr/Comment.nvim",
+  { "williamboman/mason.nvim" }, -- manages language servers
+  { "williamboman/mason-lspconfig.nvim" }, -- bridges gap between mason and lspconfig
+  { "neovim/nvim-lspconfig" }, -- sends messages from nvim to language servers
+  { 'nvim-tree/nvim-tree.lua' },
+  { 'nvim-tree/nvim-web-devicons' }, -- icons for nvimtree and barbar
+  { 'tpope/vim-fugitive' }, -- Git commands (blame, browse)
+  { 'tpope/vim-rhubarb' }, -- opens github for GBrowse
+  { "numToStr/Comment.nvim" },
   {
     'nvim-telescope/telescope.nvim', tag = '0.1.6',
     dependencies = { 'nvim-lua/plenary.nvim' }
