f0642ca3a64e3ce839d888ea0db11a66510aa9ab
MansurTsutiev <mtsutiev@movableink.com>
2024-09-07 23:23:38 -0400

    add ts server

--- lua/plugin_configs/lsp-config.lua
+++ lua/plugin_configs/lsp-config.lua
@@ -1,9 +1,10 @@
 require("mason").setup()
 require("mason-lspconfig").setup {
-  ensure_installed = { "lua_ls" },
+  ensure_installed = { "lua_ls", "tsserver" },
 }
 
 local lspconfig = require('lspconfig')
 lspconfig.lua_ls.setup({})
+lspconfig.ts_ls.setup({}) -- this uses tsserver
 vim.keymap.set({ 'n', 'v' }, '<leader>ca', vim.lsp.buf.code_action, { noremap = true, silent = false })
 
