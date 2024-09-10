03a25b5518b10272ed76a9e99fb96ffa51d9039d
MansurTsutiev <mtsutiev@movableink.com>
2024-09-09 20:14:34 -0400

    telescope live grep

--- lua/plugin_configs/telescope.lua
+++ lua/plugin_configs/telescope.lua
@@ -1,5 +1,7 @@
 local telescope = require("telescope")
+local builtin = require("telescope.builtin")
 
 telescope.setup({})
 
-vim.keymap.set("n", ",f", ":Telescope find_files<CR>", {})
+vim.keymap.set("n", "<leader>f", builtin.find_files, {})
+vim.keymap.set("n", "<leader>g", builtin.live_grep, {})
